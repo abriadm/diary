dc00ae06242636fe88a9a921cef233245b23dc72
giveonaldo <giveonaldo@gmail.com>
2024-09-01 19:51:28 +0700

    update responsive design and layout for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,81 +1,40 @@
 import { Link } from "react-router-dom";
-import { IoMenu } from "react-icons/io5";
-import { IoClose } from "react-icons/io5";
-import { useEffect, useRef, useState } from "react";
 
 function Navbar() {
-
-  const [open, isOpen] = useState(false);
-  const menuRef = useRef(null);
-
-  const handleClick = () => {
-    isOpen((prev) => !prev);
-  }
-
-  const handleClickOutSide = (event) => {
-    if (menuRef.current && !menuRef.current.contains(event.target)) {
-      isOpen(false)
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutSide);
-
-    return () => document.removeEventListener('mousedown', handleClickOutSide);
-  }, [])
-
 
   return (
     <>
       {/* Desktop */}
-      <div className="hidden sm:inline-flex bg-transparent z-20 sm:w-full justify-between font-helvetica items-center py-6 px-28">
-        <Link to="/" className="text-6xl text-[#ad9a2c] font-bold font-palmCanyon">
+      <div className="flex bg-transparent z-20 w-full justify-start sm:justify-between font-helvetica sm:items-center py-3 sm:py-6 sm:px-28">
+        <Link to="/" className="text-4xl ml-[1.7rem] sm:text-6xl text-[#597445] font-bold font-palmCanyon">
           Diary&#39;s
         </Link>
         <div className="flex sm:flex-row gap-x-6 font-semibold text-xl">
           <Link
             to="/"
-            className="transition ease-in duration-300 delay-100 hover:text-[#FF8343]"
+            className="hidden sm:block transition ease-in duration-300 delay-100 hover:text-[#FF8343]"
           >
             Home
           </Link>
           <Link
             to="/konselor"
-            className="transition ease-in duration-300 delay-100 hover:text-[#FF8343]"
+            className="hidden sm:block transition ease-in duration-300 delay-100 hover:text-[#FF8343]"
           >
             Konselor
           </Link>
           <Link
             to="/about"
-            className="transition ease-in duration-300 delay-100 hover:text-[#FF8343]"
+            className="hidden sm:block transition ease-in duration-300 delay-100 hover:text-[#FF8343]"
           >
             About
           </Link>
         </div>
         <Link
-          to="https://discord.gg/Zpa85fRy"
-          className="text-white font-semibold rounded-lg py-2 px-4 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 hover:shadow-lg duration-300"
+          to="https://discord.gg/kDBejsNU"
+          className="text-white hidden sm:block font-semibold rounded-lg py-2 px-4 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 hover:shadow-lg duration-300"
         >
           Chat
         </Link>
-      </div>
-
-      {/* Mobile */}
-      <div className="inline-flex fixed top-0 sm:hidden items-center justify-between  overflow-hidden w-[90%] py-4 mt-2">
-        <h1 className="text-4xl font-palmCanyon pl-3">Diary&#39;s</h1>
-        <button onClick={handleClick} className="transition ease-in duration-300 delay-100 hover:bg-[#FFEAC5] p-1 rounded-md text-[#603F26] hover:shadow-md"><IoMenu className="text-3xl" /></button>
-      </div>
-      <div ref={menuRef} className={`absolute z-40 transition ease-in duration-300 top-0 right-0 ${open ? 'translate-x-0' : 'translate-x-72'} w-64 bg-[#FFEAC5] text-[#603F26] h-screen`}>
-        <div className="flex items-center justify-between py-4 px-6">
-          <h1 className="font-palmCanyon text-4xl">Diary&#39;s</h1>
-          <button onClick={handleClick} className="pt-2 hover:text-black transition ease-in duration-200"><IoClose className="text-4xl" /></button>
-        </div>
-        <div className="flex flex-col mt-8 gap-y-3 px-6">
-          <Link className="text-2xl font-helvetica font-semibold transition ease-in duration-300 hover:text-black" to='/'>Home</Link>
-          <Link className="text-2xl font-helvetica font-semibold transition ease-in duration-300 hover:text-black" to='/konselor'>Konselor</Link>
-          <Link className="text-2xl font-helvetica font-semibold transition ease-in duration-300 hover:text-black" to='/about'>About</Link>
-        </div>
-        <hr className="bg-[#603F26] my-8 w-[80%] ml-6" />
       </div>
     </>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,47 @@
 import { Link } from "react-router-dom";
 import hand from "../assets/tangan.png";
+import { profile } from "../components/data";
 import { FaRegCopyright } from "react-icons/fa6";
+import { IoChatboxOutline } from "react-icons/io5";
+import anonim from "../assets/anonymous.png";
 
 function HomePage() {
+
+  const listItem = profile.map((item) => (
+    <Link
+      key={item.id}
+      to={item.url}
+      className='w-full max-w-[300px] h-fit bg-[#FCDC94] flex items-center justify-center gap-x-8 py-4 transition duration-300 ease-in hover:shadow-xl rounded-xl'
+    >
+      <img
+        src={item.img}
+        alt={item.title}
+        className='max-w-[100px] object-contain'
+      />
+      <h3 className='text-xl text-[#5C1D19] font-bold'>{item.title}</h3>
+      <IoChatboxOutline className='text-3xl font-bold text-[#FC4100]' />
+    </Link>
+  ));
+
   return (
     <>
       <div className="absolute hidden sm:block -top-6 -left-7 w-2/3 h-[115px] rounded-2xl shadow-[#DCE0FF] shadow-xl bg-[#DCE0FF] -z-10 "></div>
-      <div className="flex flex-col sm:h-auto sm:flex-row w-full mt-24 sm:mt-14 sm:justify-center items-center">
-        <div className="flex px-8 sm:px-0 flex-col sm:w-2/5">
-          <h1 className="text-4xl sm:text-6xl font-balsamiq font-extrabold mb-4 sm:mb-6 text-[#757700]">
+      <div className="flex flex-col sm:h-auto sm:flex-row w-full mt-3  sm:mt-14 sm:justify-center items-center">
+        <div className="flex w-fit mx-4 sm:mx-0 shadow-xl sm:shadow-none rounded-xl py-6 sm:py-0 px-5 sm:px-0 flex-col bg-[#F1F3C2] sm:bg-white sm:w-2/5">
+          <h1 className="text-2xl sm:text-6xl font-balsamiq font-extrabold mb-4 sm:mb-6 text-[#757700]">
             Apa itu Diary&#39;s?
           </h1>
-          <p className="font-normal font-helvetica text-lg text-justify sm:w-11/12">
+          <p className="font-normal text-[#343131] font-helvetica text-sm text-justify sm:w-11/12">
             Diary&#39;s merupakan website yang dibuat untuk memudahkan dalam berbagi
             cerita dan masalah. Dimana website ini memiliki beberapa fitur baik
             cerita secara langsung maupun anonim. Fitur ini dibuat atas dasar
             pertimbangan dan kenyamanan dari penggunaan dan tentunya terjaga
             kerahasiaannya. Ayo berbagi cerita melalui Diary&#39;s.
           </p>
-          <span className="mt-4 text-lg font-bold font-helvetica">Buka di jam 19.00 - 21.00 WIB</span>
+          <span className="mt-4 text-lg text-[#8D493A] font-bold font-helvetica">Buka di jam 19.00 - 21.00 WIB</span>
           <Link
-            to="/about"
-            className="sm:mt-6 mt-12 self-center sm:self-auto py-2 px-6 bg-[#FF8343] transition ease-in duration-300 hover:shadow-xl text-white w-fit rounded"
+            to="https://youtu.be/jRV8F8z_bKY?si=4zs_GWMyaTKXp_nU"
+            className="sm:mt-6 mt-7  self-center sm:self-auto py-2 px-6 bg-[#FF8343] transition ease-in duration-300 hover:shadow-xl text-white w-fit rounded"
           >
             Pelajari lebih lanjut
           </Link>
@@ -32,8 +52,26 @@ function HomePage() {
           className="pl-12 hidden sm:block"
           alt="tangan"
         />
+        <Link to="https://discord.gg/kDBejsNU"
+          className="w-fit mx-4 mt-10 inline-flex flex-col items-center shadow-xl sm:hidden bg-[#B1AFFF] rounded-xl overflow-hidden">
+          <img
+            src={anonim}
+            alt="anonim gambar"
+            className="block sm:hidden w-auto"
+          />
+          <h1
+            className="text-2xl my-3 font-bold text-[#FFE9D0]"
+          >
+            Anonymous Chat
+          </h1>
+        </Link>
+        <h1 id="konselor" className=" block sm:hidden mt-12 mb-5 text-4xl font-prequel text-[#8D493A] font-semibold">Konselor Sebaya</h1>
+
+        <div className="inline-flex sm:hidden flex-col w-full items-center gap-y-7">
+          {listItem}
+        </div>
       </div>
-      <div className="block sm:hidden absolute -bottom-28 left-8">
+      <div className="block sm:hidden my-10">
         <h1 className="flex items-center text-xs gap-x-2">
           <FaRegCopyright /> 2024 PPK Ormawa HIMFA, All Rights Reserved.
         </h1>
