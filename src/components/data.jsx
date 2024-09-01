71de03913387063ef416f6cc237b62de416e51e5
giveonaldo <giveonaldo@gmail.com>
2024-09-01 19:50:59 +0700

    delete data

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -3,10 +3,6 @@ import k2 from "../assets/k-2.png"
 import k3 from "../assets/k-3.png"
 import k4 from "../assets/k-4.png"
 import k5 from "../assets/k-5.png"
-import k6 from "../assets/k-6.png"
-import k7 from "../assets/k-7.png"
-import k8 from "../assets/k-8.png"
-import k9 from "../assets/k-9.png"
 
 export const profile = [
     {
@@ -37,30 +33,6 @@ export const profile = [
         id: 5,
         img: k5,
         title: "Konselor 5",
-        url: "/"
-    },
-    {
-        id: 6,
-        img: k6,
-        title: "Konselor 6",
-        url: "/"
-    },
-    {
-        id: 7,
-        img: k7,
-        title: "Konselor 7",
-        url: "/"
-    },
-    {
-        id: 8,
-        img: k8,
-        title: "Konselor 8",
-        url: "/"
-    },
-    {
-        id: 9,
-        img: k9,
-        title: "Konselor 9",
         url: "/"
     },
 ]
