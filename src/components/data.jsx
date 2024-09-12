c0542d73fc9854a1f4a1bf9e91ed89555961071d
giveonaldo <giveonaldo@gmail.com>
2024-09-12 10:23:53 +0700

    update wa.link

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,38 +1,22 @@
-import k1 from "../assets/k-1.png"
-import k2 from "../assets/k-2.png"
-import k3 from "../assets/k-3.png"
-import k4 from "../assets/k-4.png"
-import k5 from "../assets/k-5.png"
+import img from "../assets/anonim.jpeg"
 
 export const profile = [
     {
         id: 1,
-        img: k1,
+        img: img,
         title: "Konselor 1",
-        url: "https://wa.link/afrplo"
+        url: "https://wa.link/ahqygk"
     },
     {
         id: 2,
-        img: k2,
+        img: img,
         title: "Konselor 2",
-        url: "https://wa.link/1m34d0"
+        url: "https://wa.link/f377jq"
     },
     {
         id: 3,
-        img: k3,
+        img: img,
         title: "Konselor 3",
-        url: "https://wa.link/mlit0i"
-    },
-    {
-        id: 4,
-        img: k4,
-        title: "Konselor 4",
-        url: "/"
-    },
-    {
-        id: 5,
-        img: k5,
-        title: "Konselor 5",
-        url: "/"
-    },
+        url: "https://wa.link/i2cvgv"
+    }
 ]
